0633a0bf9365824ff48a276808b31bd7068b581b
Jason Frame <jason.frame@consensys.net>
2026-03-05 13:45:27 +1000

    Update Besu version 26.2.0
    Signed-off-by: Jason Frame <jason.frame@consensys.net>

--- besu.rb
+++ besu.rb
@@ -1,9 +1,9 @@
 class Besu < Formula
   desc "hyperledger besu ethereum client"
   homepage "https://github.com/hyperledger/besu"
-  url "https://github.com/hyperledger/besu/releases/download/26.1.0/besu-26.1.0.zip"
+  url "https://github.com/hyperledger/besu/releases/download/26.2.0/besu-26.2.0.zip"
   # update with: ./updateBesu.sh <new-version>
-  sha256 "356bae18a4c08a2135aa006e62a550b52428e1d613c08aa97c40ec8b908ae6cf"
+  sha256 "90b78a6739c37c6edffe362587231c51ce1b816b1f13c7aad94547e1a3a86e10"
 
   depends_on "openjdk" => "21+"
 
